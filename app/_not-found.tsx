dfc957d5a788821a7f0b226d792ffcdae3b6bf76
CloudDing <1301866313@qq.com>
2024-10-28 17:08:31 +0800

    feat: 404 界面

---
+++ app/_not-found.tsx
@@ -0,0 +1,10 @@
+// app/404.js
+export const runtime = 'edge';
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>404 - Page Not Found</h1>
+        </div>
+    );
+}
